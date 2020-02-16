869222570256710eb2c582e43bba5c8d72e35d1c
Pat Palmerston <patpalmerston@gmail.com>
2020-02-16 13:30:12 -0800

    bubble sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -27,9 +27,24 @@ def selection_sort(arr):
 
 
 # TO-DO:  implement the Bubble Sort function below
+# needs to be refactored for performance
 def bubble_sort(arr):
-
+    for i in range(len(arr)):
+        left = i
+        for j in range(i+1, len(arr)):
+            right = j
+            if arr[right] < arr[left]:
+                arr[right], arr[left] = arr[left], arr[right]
     return arr
+
+    '''
+    with bubble we need two pointers, left index and right index that increment forward together
+    left starts at 0 index, right starts at 1
+    Outer loop starts with our 0 index
+    inner loop starts with our second index
+    as we run the inner loop against the outer loop we check to see if the j index is less than i. if it is then we swap and return arr
+
+    '''
 
 
 # STRETCH: implement the Count Sort function below
